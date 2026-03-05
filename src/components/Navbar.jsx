0ee5f8c5a07c991891d144c463ae347bc99f9243
Hugo Leonardo <hugoleonardopflorencio@gmail.com>
2026-03-05 14:28:16 -0300

    OMG DELEVOPER AGAIN

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-18px font-bold cursor-pointer flex">
             Hugo&nbsp;
-            <span className="sm:block hidden">| Fullstack Delevoper</span>
+            <span className="sm:block hidden">| Fullstack Developer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
